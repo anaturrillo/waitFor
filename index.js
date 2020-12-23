ca24d54adceb83fc5e8b278bc38f01dcdd3c293f
Ana Turrillo <anaturrillo@gmail.com>
2020-12-23 16:10:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+exports.all = promises => Promise.all(promises);
+
+exports.wait = (obj) => {
+  if (
+    !obj ||
+    obj instanceof Promise ||
+    typeof obj !== 'object' ||
+    typeof obj[Symbol.iterator] === 'function'
+  ) {
+    throw new TypeError('No object received ' + obj);
+  }
+  return Promise.all(Object.keys(obj).map(key => Promise.all([key, obj[key]]))).then(
+    values =>
+      values.reduce(
+        (response, [key, value]) => ({...response, [key]: value}),
+        {}
+      )
+  );
+};
